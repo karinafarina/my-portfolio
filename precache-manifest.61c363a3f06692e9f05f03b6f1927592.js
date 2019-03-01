7a5496f6011e57010ba4f7ca9b7f168f24f905be
Karina Gaulin <karina@domandtom.com>
2019-03-01 13:31:00 -0500

    Updates

---
+++ precache-manifest.61c363a3f06692e9f05f03b6f1927592.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ada8ec26695c3fb965bcdfa53a002058",
+    "url": "/my-portfolio/static/media/breastfeeding.ada8ec26.png"
+  },
+  {
+    "revision": "e97ae92304db0308f5bd",
+    "url": "/my-portfolio/static/css/main.262f574c.chunk.css"
+  },
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "2602b417bbfef9f92e56",
+    "url": "/my-portfolio/static/js/2.2602b417.chunk.js"
+  },
+  {
+    "revision": "865d2c6ead36c4542cc40bd4611555d6",
+    "url": "/my-portfolio/static/media/me3.865d2c6e.png"
+  },
+  {
+    "revision": "a1d80825c748d7bcffe5312074055224",
+    "url": "/my-portfolio/static/media/clouds2.a1d80825.ogv"
+  },
+  {
+    "revision": "d5164e14674be483192cd539753d812d",
+    "url": "/my-portfolio/static/media/astronomy.d5164e14.png"
+  },
+  {
+    "revision": "e97ae92304db0308f5bd",
+    "url": "/my-portfolio/static/js/main.e97ae923.chunk.js"
+  },
+  {
+    "revision": "cb4536b5974876c2a290a8a4dee16ac0",
+    "url": "/my-portfolio/static/media/nexter.cb4536b5.png"
+  },
+  {
+    "revision": "b9cc2d6fcd6cc8874a519fb78263782b",
+    "url": "/my-portfolio/static/media/trillo.b9cc2d6f.png"
+  },
+  {
+    "revision": "8dc04bcddc1ee128393621e994aa53e9",
+    "url": "/my-portfolio/static/media/linkedin.8dc04bcd.svg"
+  },
+  {
+    "revision": "8a1ba32d3c7b69b57d05b74cd65d8bd8",
+    "url": "/my-portfolio/static/media/Github.8a1ba32d.svg"
+  },
+  {
+    "revision": "8de2881eddd3b0250dc6f52f57292eda",
+    "url": "/my-portfolio/static/media/Gmail.8de2881e.svg"
+  },
+  {
+    "revision": "1834a3a81a8fae8b55e1c310d6f4471f",
+    "url": "/my-portfolio/index.html"
+  }
+];
