c9e4548b81267eaf2257d71eea38a640a08ecc16
Karina Gaulin <karina@domandtom.com>
2019-03-05 10:26:22 -0500

    Change font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 
-
-
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="my-brand">Karina Gaulin</div>
+        {/* <div className="my-brand">Karina Gaulin</div>
         <ul className="nav">
           <li>Home</li>
           <li className="resume">
             <a href="https://www.resume.com/share/karinagaulin" download>Resume</a>
           </li>
           <li><a href="mailto:karinagaulin@gmail.com">Connect</a></li>
-        </ul>
+        </ul> */}
       </div>
       );
     }
