9d398be07d612f9585c609e9273f84b0a7b760a1
Karina Gaulin <karina@domandtom.com>
2019-03-06 12:53:56 -0500

    Add hamburger toggle to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap'
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        {/* <div className="my-brand">Karina Gaulin</div>
-        <ul className="nav">
-          <li>Home</li>
-          <li className="resume">
-            <a href="https://www.resume.com/share/karinagaulin" download>Resume</a>
-          </li>
-          <li><a href="mailto:karinagaulin@gmail.com">Connect</a></li>
-        </ul> */}
+        <Navbar className="my-navbar" variant="dark" expand="lg">
+          <Navbar.Brand className="my-brand" href="#home">Karina Gaulin</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="dropdown" id="basic-navbar-nav">
+            <Nav className="mr-auto menu">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="https://www.resume.com/share/karinagaulin" download>Resume</Nav.Link>
+              <Nav.Link href="mailto:karinagaulin@gmail.com">Connect</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
       );
     }
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Omnifood from "../images/omnifood.png";
 import Nexter from "../images/nexter.png";
 import Trillo from "../images/trillo.png";
-import Breatfeeding from "../images/breastfeeding.png"
+import Astronomy from "../images/astronomy2.png"
 //import { Link } from 'react-router';
 
 class Showcase extends Component {
@@ -10,7 +10,7 @@ class Showcase extends Component {
   render() {
     return (
       <div className="showcase">
-        <h1 className="title text-center py-3">My Work</h1>
+        <h1 className="title text-center py-3">Featured Work</h1>
         <div className="projects">
           <div className="project project-1">
             <a href="https://karinafarina.github.io/MyNextor/" alt="Nexter">
@@ -23,7 +23,7 @@ class Showcase extends Component {
             </a>
           </div>
           <div className="project project-3">
-            <a href="https://karinafarina.github.io/Breastfeeding-Quiz/"><img src={Breatfeeding} alt="Woman breastfeeding"/></a>
+            <a href="https://karinafarina.github.io/Astronomy-Aleworks/"><img src={Astronomy} alt="Astronomy Aleworks"/></a>
           </div>
           <div className="project project-4">
             <a href="https://karinafarina.github.io/Flexbox/">
