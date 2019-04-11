4314f363a1923ac55bba4a59e656dbda5b111ab9
Karina Gaulin <karina@domandtom.com>
2019-04-11 10:42:06 -0400

    Add height 100%;

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,20 @@ import { Navbar, Nav } from 'react-bootstrap';
 class Header extends Component {
 
 
-  blockBodyScroll = () => {
-    document.body.classList.add('block-body-scroll');
-
-  }
-
-  allowBodyScroll = () => {
-    document.body.classList.remove('block-body-scroll');
-  }
+  // blockBodyScroll = () => {
+  //   document.body.classList.add('block-body-scroll');
+  //
+  // }
+  //
+  // allowBodyScroll = () => {
+  //   document.body.classList.remove('block-body-scroll');
+  // }
 
 
   render() {
     return (
       <div className="header">
-        <Navbar collapseOnSelect fixed="top" className="my-navbar" variant="dark" expand="lg">
+        <Navbar collapseOnSelect className="my-navbar" variant="dark" expand="lg">
           <Navbar.Brand className="my-brand" href="#home">Karina Gaulin</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="dropdown" id="basic-navbar-nav">
